e0b80c9461ffc97202c8cc7deed58bbc074d275b
Kakezu <kaius.vanajas@student.laurea.fi>
2023-04-12 13:34:20 +0300

    first commit, firebase link successful

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'labwork-4-9e154',
+    appId: '1:454348359700:web:b13f333d5dc47fbfb47f01',
+    storageBucket: 'labwork-4-9e154.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyATcBmi2NauWUIUEoqUiAMpRj2_PjhABUg',
+    authDomain: 'labwork-4-9e154.firebaseapp.com',
+    messagingSenderId: '454348359700',
+    measurementId: 'G-QE89YNN2BS',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'labwork-4-9e154',
+    appId: '1:454348359700:web:b13f333d5dc47fbfb47f01',
+    storageBucket: 'labwork-4-9e154.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyATcBmi2NauWUIUEoqUiAMpRj2_PjhABUg',
+    authDomain: 'labwork-4-9e154.firebaseapp.com',
+    messagingSenderId: '454348359700',
+    measurementId: 'G-QE89YNN2BS',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
